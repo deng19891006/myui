cbf1b4f15e1fb35f28fc91d9ec99aa887421c794
deng19891006 <540423724@qq.com>
2013-10-25 17:44:44 +0800

    update datepicker.html datepicker.js

--- component/datepicker/js/datepicker.js
+++ component/datepicker/js/datepicker.js
@@ -12,20 +12,34 @@
 		docu = win.document;
 
 	var defaults = {
-			 
+			 element : '',   //触发元素
+			 monthNum : 1,	 //显示月份数量
+			 range : false,  //是否带有日期范围可控功能，默认无
+			 initDate : ''   //初始化date,默认与本地date一致
 	}
 
 	var datepicker = function( options ){
 
-		if( !(this instanceof tip) ){
-			return new tip( options );
+		if( !(this instanceof datepicker) ){
+			return new datepicker( options );
 		}
 
 		options = $.extend( defaults , options );
 		this.__o__ = options ;
-		myuiTip.init( options );
+		myDatepicker.init( options );
+
 	}
 
+	var myDatepicker = {
+		'init' : function ( options ){
+
+		}
+	}
+
+	myDatepicker.prototype = {
+
+	}
+	
 	exports.datepicker = datepicker;
 
  });
