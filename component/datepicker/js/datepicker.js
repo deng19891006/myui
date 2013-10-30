51ddd56a1f045c9114492270cf98e76a019fb903
deng19891006 <540423724@qq.com>
2013-10-30 15:33:53 +0800

    add severl Date function

--- component/datepicker/js/datepicker.js
+++ component/datepicker/js/datepicker.js
@@ -54,7 +54,65 @@
 				wrapDom.innerHTML = wrapStr.join('');
 			this.datepickerWrap = docu.createDocumentFragment();
 			this.datepickerWrap.appendChild(wrapDom);
-			docu.body.appendChild(this.datepickerWrap);
+			this.datepickerWrap_datepList = this.datepickerWrap.childNodes[0].childNodes[0].lastChild;
+			// this.datepickerWrap_datepList.appendChild( myDatepicker.loadDateWrap() );
+			//docu.body.appendChild(this.datepickerWrap);
+			console.log(myDatepicker.getDaysNumForMonth(2013,2));
+		},
+
+		/*
+		* 得到当前年份
+		* @return { Number } 年份
+		*/
+		'getCurrnetYear' : function(){
+			return new Date().getFullYear();
+		},
+
+		/*
+		* 得到当前月份
+		* @return { Number } 月份 1-12
+		*/
+		'getCurrnetMonth' : function(){
+			return new Date().getMonth()+1;
+		},
+
+		/*
+		* 得到当前月份
+		* @return { Number } 天数 1-31
+		*/
+		'getCurrnetDay' : function(){
+			return new Date().getDate();
+		},
+
+		/*
+		* 得到每个月1号的day(礼拜数)
+		* @y { Number } 年份
+		* @m { Number } 月份
+		* @return { Number } 1-7
+		*/
+		'getFirstDayForEverymonth' : function( y , m ){
+			var myDate=new Date();
+			myDate.setFullYear(y);
+			myDate.setMonth(m-1);
+			myDate.setDate(1);
+			return myDate.getDay();
+		},
+
+		/*
+		* 得到每个月的天数
+		* @y { Number } 年份
+		* @m { Number } 月份
+		* @return { Number } 28||29||30||31
+		*/
+		'getDaysNumForMonth' : function( y , m ){
+			var isLeapYear = y%4 === 0 ? true : false;
+			if( m===2 ){
+				return isLeapYear ? 29 : 28;
+			}else if( m <=7 ){
+				return m%2 === 0 ? 30 : 31;
+			}else{
+				return m%2 === 0 ? 31 : 30;
+			}
 		}
 	}
 
